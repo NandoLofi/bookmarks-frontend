8071ca505510a2aa4b632042b299159f7e587c42
Fernando Galvan <n.brighter00@gmail.com>
2022-06-01 22:46:18 -0500

    adding functions to appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './components/Header'
-import {useState, useEffect} from 'react'
 import Main from './components/Main';
+import Bookmark from './pages/Bookmark';
+import Index from './pages/Index';
+import New from './pages/New';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
+      <Bookmark/>
+      <Index />
+      <New />
 
     </div>
   );
