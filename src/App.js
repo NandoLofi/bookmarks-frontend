e0b80ddfbe5dd3a45f7439068fb4dde0d19809d8
Fernando Galvan <n.brighter00@gmail.com>
2022-06-01 22:35:53 -0500

    adjusting components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,6 @@ import {useState, useEffect} from 'react'
 import Main from './components/Main';
 
 function App() {
-  const [state, setState] = useState()
-
-useEffect(()=>{
-  async function getBookmarks(){
-    try{
-      let mark = await fetch ('https://bookmarked-backend2022.herokuapp.com/bookmark')
-      .then(res=> res.json())
-      setState(mark)
-
-    }catch(err){
-      console.log(err)
-    }
-  }
-})
-
-function addBookmark(evt){
-  evt.preventDefault();
-  
-}
-
-
   return (
     <div className="App">
       <Header />
